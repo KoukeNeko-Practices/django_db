6930f61310622938d8a9be7075c3b05d1237b5fb
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2023-12-26 14:42:11 +0800

    add sql comment

--- app/views.py
+++ app/views.py
@@ -7,15 +7,15 @@ from .models import Message
 
 def create_user(request):
     # Example of creating a user
-    user = User.objects.create_user('john_doe', 'john@example.com', 'johnpassword')
-    user.save()
+    user = User.objects.create_user('john_doe', 'john@example.com', 'johnpassword') # SQL: INSERT INTO User (username, email, password) VALUES (...)
+    user.save() # SQL: UPDATE [table] SET [fields] WHERE [condition]
     return HttpResponse("User created successfully")
 
 def create_message(request):
     user = User.objects.first()  # Get the first user
     if user:
         message = Message(user=user, text='Hello, world!')
-        message.save()
+        message.save() # SQL: UPDATE [table] SET [fields] WHERE [condition]
         return HttpResponse("Message created successfully")
     else:
         return HttpResponse("No users available to attach a message")
@@ -29,7 +29,7 @@ def add_message(request):
     if request.method == 'POST':
         message_text = request.POST.get('message_text')
         if message_text:
-            Message.objects.create(user=request.user, text=message_text)
+            Message.objects.create(user=request.user, text=message_text) # SQL: INSERT INTO Message (user, text) VALUES message_text
         return redirect('/')  # Redirect to the index page or a confirmation page
     return redirect('/')  # Handle non-POST requests
 
@@ -41,7 +41,7 @@ def edit_message(request, message_id):
         message = get_object_or_404(Message, id=message_id, user=request.user)
     if request.method == 'POST':
         message.text = request.POST.get('edited_message', '')
-        message.save()
+        message.save() # SQL: UPDATE Message SET text
         return redirect('/')
     return redirect('/')
 
@@ -52,21 +52,21 @@ def delete_message(request, message_id):
     else:
         message = get_object_or_404(Message, id=message_id, user=request.user)
     if request.method == 'POST':
-        message.delete()
+        message.delete() # SQL: DELETE FROM Message WHERE id=message_id
         return redirect('/')
     else:
         return HttpResponse("Message delete fail")
 
 @login_required
 def my_messages(request):
-    user_messages = Message.objects.filter(user=request.user).order_by('-created_at')
+    user_messages = Message.objects.filter(user=request.user).order_by('-created_at') # SQL: SELECT * FROM Message WHERE user=request.user
     return render(request, 'index.html', {'messages': user_messages})
 
 def search_view(request):
     if request.method == "POST":
         query = request.POST.get('search', '')  # Get the search query from POST data
         if query:
-            results = Message.objects.filter(text__icontains=query)
+            results = Message.objects.filter(text__icontains=query) # SQL: SELECT * FROM Message WHERE text LIKE '%query%'
         else:
             results = Message.objects.none()  # Empty queryset
     else:
@@ -87,7 +87,7 @@ def login(request):
 
 def index(request):
     # Retrieve all message objects from the database
-    messages = Message.objects.all().order_by('-created_at')
+    messages = Message.objects.all().order_by('-created_at') # SQL: SELECT * FROM Message
 
     # Pass messages to the template
     context = {
